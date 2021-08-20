766f6957d06a90c4e602ac8b8bc917b8675b82f9
Corey <corey@dolomite.io>
2021-08-20 05:22:48 -0400

    bumped version, fixed FATEx logo

--- src/buildList.js
+++ src/buildList.js
@@ -1,9 +1,4 @@
 const { version } = require("../package.json");
-/*const mainnet = require("./tokens/mainnet.json");
-const ropsten = require("./tokens/ropsten.json");
-const rinkeby = require("./tokens/rinkeby.json");
-const goerli = require("./tokens/goerli.json");
-const kovan = require("./tokens/kovan.json");*/
 const bscMainnet = require("./tokens/bsc-mainnet.json");
 const bscTestnet = require("./tokens/bsc-testnet.json");
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
@@ -12,7 +7,7 @@ const harmonyTestnet = require("./tokens/harmony-testnet.json");
 module.exports = function buildList() {
   const parsed = version.split(".");
   return {
-    name: "Venomswap Default",
+    name: "FATEx Default",
     timestamp: new Date().toISOString(),
     version: {
       major: +parsed[0],
@@ -20,8 +15,8 @@ module.exports = function buildList() {
       patch: +parsed[2],
     },
     tags: {},
-    logoURI: "https://dvwecb5klcqus.cloudfront.net/venomswap/logos/venomswap-128x128.png",
-    keywords: ["venomswap", "default"],
+    logoURI: "https://fatex.io/fatex-token-logo-large.png",
+    keywords: ["fatex", "default"],
     tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
