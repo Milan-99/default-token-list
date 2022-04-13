33a7ee5aff9f85b7b3a73df3db396f91c7a336a9
Corey <corey@dolomite.io>
2022-04-13 04:25:21 -0400

    added polygon lists

--- src/buildList.js
+++ src/buildList.js
@@ -3,6 +3,7 @@ const bscMainnet = require("./tokens/bsc-mainnet.json");
 const bscTestnet = require("./tokens/bsc-testnet.json");
 const harmonyMainnet = require("./tokens/harmony-mainnet.json");
 const harmonyTestnet = require("./tokens/harmony-testnet.json");
+const polygonMainnet = require("./tokens/polygon-mainnet.json");
 
 module.exports = function buildList() {
   const parsed = version.split(".");
@@ -17,7 +18,7 @@ module.exports = function buildList() {
     tags: {},
     logoURI: "https://fatex.io/fatex-token-logo-large.png",
     keywords: ["fatex", "default"],
-    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet]
+    tokens: [...bscMainnet, ...bscTestnet, ...harmonyMainnet, ...harmonyTestnet, ...polygonMainnet]
       // sort them by symbol for easy readability
       .sort((t1, t2) => {
         if (t1.chainId === t2.chainId) {
